deb6f97c9f00d3dbb8c25ca0be2b649a3f44e5bb
allexandrelucas0-blip <allexandrelucas0@gmail.com>
2026-05-21 18:38:37 +0000

    inicios de fundamentos de javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+//isso aqui é um comentário
+
+/**
+ * Este é um
+ * comentário de
+ * múltipas linhas
+ */
+
+var nome ;
+let data_nascimento = "2009-02-27"
+ //const CPF = 0000000000;
+
+ let num1 = 2;
+ let num2 = 5;
+
+ let soma = num1 + num2;
+
+ console.log(soma);
+
+// conectivo lógico? E OU NÂO
+// AND OR NOT - && || !
+
+if (!nome){
+    console.log("Não está preenchido")
+}
